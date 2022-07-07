b5ee20bc5451f1618341eb0004224c8339e4b0d5
Wajih <hass_wajih@hotmail.com>
2022-07-07 17:03:35 -0400

    Db Schema

---
+++ backend/migrations/20220707184553-create-articles.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('articles', {
+      
+      article_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('articles');
+  }
+};
