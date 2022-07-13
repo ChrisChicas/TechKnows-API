3aea4aa4c462c5ffaf1281f4799f43644af6e688
Chris Chicas <christianchicas7@gmail.com>
2022-07-13 16:07:38 -0400

    test

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cookieSession({
     name: 'session',
     keys: [ process.env.SESSION_SECRET ],
     maxAge: 24* 60 * 1000,
+    httpOnly: true
     sameSite: "none",
     secure: true
 }))
