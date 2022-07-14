41ef5d1440de58a538ca855c8c1c23e3060b92db
Chris Chicas <christianchicas7@gmail.com>
2022-07-13 23:07:35 -0400

    limited info returns from database

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,9 +8,9 @@ const { User } = db
   
 router.post('/', async (req, res) => {
     let user = await User.findOne({
-        where: { username: req.body.username }
+        where: { username: req.body.username },
+        attributes: ["user_id"]
     })
-
     if (!user || !await bcrypt.compare(req.body.password, user.password)) {
         return res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
